af77e96e04841f55061d29449a49a73b78eb2186
cplassais <plassais.christophe@gmail.com>
2021-11-03 16:46:07 +0100

    03-11-21 46:45

---
+++ src/Controller/ContentController.php
@@ -0,0 +1,14 @@
+<?php
+// src/Controller/ContentController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContentController extends AbstractController {
+    public function display(string $title): Response {
+        return $this->render('content/content.html.twig', [
+            'title' => $title,
+            'description' => 'contenu de la page'
+        ]);
+    }
+}
