2a4fffbfb0481c88f2406e7005c876d6e5f33b78
cplassais <plassais.christophe@gmail.com>
2021-11-16 14:40:36 +0100

    16-11-2021 14:40 ok

--- src/Form/Type/ProductFormType.php
+++ src/Form/Type/ProductFormType.php
@@ -3,9 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Brand;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class ProductFormType extends AbstractType
 {
@@ -15,6 +20,19 @@ class ProductFormType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
+            ->add('new', CheckboxType::class,['required'=>false])
+            ->add('brand', EntityType::class, [
+                'class' => Brand::class,
+                'choice_label'=>'name',
+                'required' => true
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'multiple' => true,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'required' => true
+            ])
         ;
     }
 
